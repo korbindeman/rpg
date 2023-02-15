c6daf192c71eb215b13eb9986b52b3782b2be843
Korbin de Man <113640462+korbindeman@users.noreply.github.com>
2023-02-15 16:19:25 +0100

    implemented moving mechanics

--- Player.cs
+++ Player.cs
@@ -8,16 +8,43 @@ class Player
     public int Level;
     public Weapon CurrentWeapon;
     public Location CurrentLocation;
-    public QuestList QuestLog;
-    public Inventory CountedItemList;
+    // public QuestList QuestLog;
+    // public Inventory CountedItemList;
 
-    public Player()
+    public Player(string name)
     {
-
+        Name = name;
+        CurrentHitPoints = 10;
+        MaximumHitPoints = 20;
+        Gold = 10;
+        ExperiencePoints = 0;
+        Level = 1;
+        CurrentWeapon = World.WeaponByID(1);
+        CurrentLocation = World.LocationByID(1);
     }
 
-    public bool Move(string direction)
+    public Location? Move(string direction)
     {
-        return 
+        Location? targetLocation = null;
+        switch (direction)
+        {
+            case "north":
+                targetLocation = CurrentLocation.LocationToNorth;
+                break;
+            case "east":
+                targetLocation = CurrentLocation.LocationToEast;
+                break;
+            case "south":
+                targetLocation = CurrentLocation.LocationToSouth;
+                break;
+            case "west":
+                targetLocation = CurrentLocation.LocationToWest;
+                break;
+        }
+        if (targetLocation is null) return null;
+
+        CurrentLocation = targetLocation;
+
+        return targetLocation;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 namespace rpg;
 class Program
 {
+    static Player p = new Player("Korbin");
+
     static void Main(string[] args)
     {
+
         while (true)
         {
             Console.Write("> ");
@@ -15,6 +18,26 @@ class Program
     static void ProcessCommand(string? command)
     {
         Console.WriteLine("command {0}", command);
+        if (command == "n")
+        {
+            Location? newLocation = p.Move("north");
+            Console.WriteLine("Moved to {0}", newLocation.Name);
+        }
+        if (command == "e")
+        {
+            Location? newLocation = p.Move("east`");
+            Console.WriteLine("Moved to {0}", newLocation.Name);
+        }
+        if (command == "s")
+        {
+            Location? newLocation = p.Move("south");
+            Console.WriteLine("Moved to {0}", newLocation.Name);
+        }
+        if (command == "w")
+        {
+            Location? newLocation = p.Move("west");
+            Console.WriteLine("Moved to {0}", newLocation.Name);
+        }
         if (command == "quit")
         {
             Environment.Exit(0);
