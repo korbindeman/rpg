d4bf8d654bae6f4f08e9cbb2e6766b9c2aa6039d
Korbin de Man <113640462+korbindeman@users.noreply.github.com>
2023-02-15 15:07:35 +0100

    updated .gitignore

---
+++ Player.cs
@@ -0,0 +1,23 @@
+class Player
+{
+    public string Name;
+    public int CurrentHitPoints;
+    public int MaximumHitPoints;
+    public int Gold;
+    public int ExperiencePoints;
+    public int Level;
+    public Weapon CurrentWeapon;
+    public Location CurrentLocation;
+    public QuestList QuestLog;
+    public Inventory CountedItemList;
+
+    public Player()
+    {
+
+    }
+
+    public bool Move(string direction)
+    {
+        return 
+    }
+}
